997657c109bdd1c6f52b53eff97b1eb7df9dd930
Vaibhav <vaibhavagarwal693@gmail.com>
2023-05-21 23:11:59 +0530

    no change

---
+++ backend/models/file.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  filename: {
+    type: String,
+    required: true,
+  },
+ userId:{
+    type :mongoose.Schema.Types.ObjectId,
+    ref: "User" ,
+ },
+ companyId:{
+    type :mongoose.Schema.Types.ObjectId,
+    ref: "Company" ,
+ }
+  
+});
+
+export const File = mongoose.model("File", schema);
